a20e75fadac24099289770fa05e9062693665356
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-03-01 23:58:48 -0800

    The Markov chain Markovs
    This is one I can finally markov the to-do list

--- markov.py
+++ markov.py
@@ -1,13 +1,15 @@
 from dictogram import Dictogram
+import random
+from numpy.random import choice
 
 
 class MarkovChain:
 
     def __init__(self, word_list):
-
         # The Markov chain will be a dictionary of dictionaries
         # Example: for "one fish two fish red fish blue fish"
-        # {"one": {fish:1}, "fish": {"two":1, "red":1, "blue":1}, "two": {"fish":1}, "red": {"fish":1}, "blue": {"fish:1"}}
+        # {"one": {fish:1}, "fish": {"two":1, "red":1, "blue":1}, "two":
+        # {"fish":1}, "red": {"fish":1}, "blue": {"fish:1"}}
         self.markov_chain = self.build_markov(word_list)
         self.first_word = list(self.markov_chain.keys())[0]
 
@@ -19,11 +21,11 @@ class MarkovChain:
             current_word = word_list[i]
             next_word = word_list[i+1]
 
-            if current_word in markov_chain.keys(): # already there
+            if current_word in markov_chain.keys():  # already there
                 # get the histogram for that word in the chain
                 histogram = markov_chain[current_word]
                 # add to count
-                histogram.dictionary_histogram[next_word] = histogram.dictionary_histogram.get(next_word, 0) + 1
+                histogram[next_word] = histogram.get(next_word, 0) + 1
             else:  # first entry
                 markov_chain[current_word] = Dictogram([next_word])
 
@@ -31,13 +33,37 @@ class MarkovChain:
 
     def walk(self, num_words):
         # TODO: generate a sentence num_words long using the markov chain
-        pass
+        sentence = [random.choice(list(self.markov_chain.keys()))]
+        print(sentence[0])
+        for i in range(1, num_words):
+            probability_dict = self.markov_chain[sentence[i-1]]
+            print("Probabilities for target word " + sentence[i-1] + ": ")
+            print(probability_dict)
+
+            prob_list = []
+            num_words = 0
+            for num in list(probability_dict.values()):
+                num_words += num
+
+            for num in list(probability_dict.values()):
+                prob_list.append(num / num_words)
+
+            print(prob_list)
+
+            print(list(probability_dict.keys()))
+            print(list(probability_dict.values()))
+
+            # print(choice(list(probability_dict.keys()), 1, p=list(probability_dict.values()))[0])
+
+            sentence.append(choice(list(probability_dict.keys()), 1, p=prob_list)[0])
+            print(sentence)
 
     def print_chain(self):
         for word, histogram in self.markov_chain.items():
             print(word, histogram.dictionary_histogram)
 
 
-markov_chain = MarkovChain(["one", "fish", "two", "fish", "red", "fish", "blue", "fish"])
-markov_chain.print_chain()
-print(markov_chain.walk(10))
+markov_chain = MarkovChain(["one", "fish", "two", "fish", "red", "fish", "blue", "fish", "fish", "two", "fish"])
+markov_chain.walk(5)
+# markov_chain.print_chain()
+# print(markov_chain.walk(10))
