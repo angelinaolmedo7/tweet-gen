bea4323126acadc0a99e5774144de7eb9107e8ef
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-03-02 01:45:53 -0800

    Cleaned up markov

--- markov.py
+++ markov.py
@@ -1,6 +1,7 @@
 from dictogram import Dictogram
 import random
 from numpy.random import choice
+import string
 
 
 class MarkovChain:
@@ -34,11 +35,8 @@ class MarkovChain:
     def walk(self, num_words):
         # TODO: generate a sentence num_words long using the markov chain
         sentence = [random.choice(list(self.markov_chain.keys()))]
-        print(sentence[0])
         for i in range(1, num_words):
             probability_dict = self.markov_chain[sentence[i-1]]
-            print("Probabilities for target word " + sentence[i-1] + ": ")
-            print(probability_dict)
 
             prob_list = []
             num_words = 0
@@ -48,22 +46,35 @@ class MarkovChain:
             for num in list(probability_dict.values()):
                 prob_list.append(num / num_words)
 
-            print(prob_list)
-
-            print(list(probability_dict.keys()))
-            print(list(probability_dict.values()))
-
-            # print(choice(list(probability_dict.keys()), 1, p=list(probability_dict.values()))[0])
-
             sentence.append(choice(list(probability_dict.keys()), 1, p=prob_list)[0])
-            print(sentence)
+
+        simple_sentence = ""
+        for i in sentence:
+            simple_sentence += i + " "
+        simple_sentence = simple_sentence.capitalize().strip() + "."
+        return simple_sentence
 
     def print_chain(self):
         for word, histogram in self.markov_chain.items():
             print(word, histogram.dictionary_histogram)
 
+    @staticmethod
+    def read_text_file(fileName):
+        textFile = open(fileName, "r")
+        fileLines = textFile.readlines()
+        words_list = []
 
-markov_chain = MarkovChain(["one", "fish", "two", "fish", "red", "fish", "blue", "fish", "fish", "two", "fish"])
-markov_chain.walk(5)
+        for line in fileLines:
+            for word in line.split():
+                # strip punctuation/capitals
+                i = word.lower().translate(
+                    str.maketrans('', '', string.punctuation))
+                words_list.append(i)
+        return words_list
+
+
+markov_chain = MarkovChain(["one", "fish", "two", "fish", "red", "fish", "blue", "fish"])
+taz_chain = MarkovChain(MarkovChain.read_text_file("taz_balance_10.txt"))
+# markov_chain.walk(5)
 # markov_chain.print_chain()
-# print(markov_chain.walk(10))
+print(taz_chain.walk(10))
